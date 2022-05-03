aff3eb68d175fac976d5f4fcc8ef421196795581
TayfunF <firtinatayfun08@gmail.com>
2022-05-03 12:24:26 +0300

    Core → IService Created

---
+++ NTierArchitect.Core/Services/IService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTierArchitect.Core.Services
+{
+    public interface IService<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+
+        Task<IEnumerable<T>> GetAllAsync();
+
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+
+        Task<T> AddAsync(T entity);
+
+        Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
+
+        Task UpdateAsync(T entity);
+
+        Task RemoveAsync(T entity);
+
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+    }
+}
