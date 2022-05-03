3904dc2fd3e5f43439cb9d3a3f8f9154f31d8891
TayfunF <firtinatayfun08@gmail.com>
2022-05-03 12:33:23 +0300

    Repository → AppDbContext Created

---
+++ NTierArchitect.Repository/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NTierArchitect.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTierArchitect.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+    }
+}
