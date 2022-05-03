d63542823c3889bd56c3b2503767efc122991945
TayfunF <firtinatayfun08@gmail.com>
2022-05-03 14:28:30 +0300

    Service → AutoMapper Created

---
+++ NTierArchitect.Service/Mapping/MapProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using NTierArchitect.Core.Dtos;
+using NTierArchitect.Core.Models;
+
+namespace NTierArchitect.Service.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<CategoryDto, Category>().ReverseMap();
+            CreateMap<ProductFeatureDto, ProductFeature>().ReverseMap();
+        }
+    }
+}
