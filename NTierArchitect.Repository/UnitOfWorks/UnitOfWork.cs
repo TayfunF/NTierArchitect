b80fdeae591fd0cdc6e7e3e2160467c629e810f4
TayfunF <firtinatayfun08@gmail.com>
2022-05-03 13:01:12 +0300

    Repository → UnitOfWork Created

---
+++ NTierArchitect.Repository/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using NTierArchitect.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTierArchitect.Repository.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext Context;
+        public UnitOfWork(AppDbContext context)
+        {
+            Context = context;
+        }
+
+        public void Commit()
+        {
+            Context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await Context.SaveChangesAsync();
+        }
+    }
+}
