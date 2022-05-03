e6c72de491cb87a0f682e40cd0b0baa1fc4708c2
TayfunF <firtinatayfun08@gmail.com>
2022-05-03 12:15:32 +0300

    Core → Models Created

---
+++ NTierArchitect.Core/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTierArchitect.Core.Models
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
