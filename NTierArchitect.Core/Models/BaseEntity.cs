4f79af7f87c4dfd87b32413c64889e5f545c2e33
TayfunF <firtinatayfun08@gmail.com>
2022-05-03 13:16:27 +0300

    Core → DateTime Prop Updated

--- NTierArchitect.Core/Models/BaseEntity.cs
+++ NTierArchitect.Core/Models/BaseEntity.cs
@@ -9,7 +9,7 @@ namespace NTierArchitect.Core.Models
     public class BaseEntity
     {
         public int Id { get; set; }
-        public DateTime CreatedDate { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
         public DateTime? UpdatedDate { get; set; }
     }
 }
