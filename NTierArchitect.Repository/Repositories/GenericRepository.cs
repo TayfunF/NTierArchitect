982509a2a7dffcf72c514da630f88a14db09c58a
TayfunF <firtinatayfun08@gmail.com>
2022-05-03 12:58:17 +0300

    Repository → GenericRepository Created

---
+++ NTierArchitect.Repository/Repositories/GenericRepository.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NTierArchitect.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTierArchitect.Repository.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly AppDbContext Context;
+        private readonly DbSet<T> DbSet;
+
+        public GenericRepository(AppDbContext context)
+        {
+            Context = context;
+            DbSet = Context.Set<T>();
+        }
+        public async Task AddAsync(T entity)
+        {
+            await DbSet.AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await DbSet.AddRangeAsync(entities);
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> expression)
+        {
+            return await DbSet.AnyAsync(expression);
+        }
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> expression)
+        {
+            return DbSet.AsNoTracking().AsQueryable();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public void Remove(T entity)
+        {
+            DbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            DbSet.RemoveRange(entities);
+        }
+
+        public void Update(T entity)
+        {
+            DbSet.Update(entity);
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> expression)
+        {
+            return DbSet.Where(expression);
+        }
+    }
+}
